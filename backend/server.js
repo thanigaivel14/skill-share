c7214798a677c44146bfbacbe35b794c4539dad8
thanigai14 <thanigai905@gmail.com>
2026-05-07 17:19:35 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 
 const io = new Server(server, {
+  cors: {
+    origin: process.env.CLT_URL,
+    credentials: true
+  }
 })
 
 const onlineUsers = new Map();
